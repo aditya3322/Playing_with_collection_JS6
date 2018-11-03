b9d8ef3bfb20c96302a98c2ccdea9a210a15f44c
Aditya Awasthi <aditya3322@gmail.com>
2018-11-03 14:12:39 +0530

    JS collections problrems

--- contstruct-array-without-duplicate.js
+++ contstruct-array-without-duplicate.js
@@ -3,7 +3,6 @@ let numbers = [10, 20, 20, 40, 10, 60, 60, 80, 70, 10];
 
 let constructArrWithoutDuplicate = function() {
 	let duplicates = [];
-
 	numbers.forEach(num => {
 		if(duplicates[`item_${num}`] == undefined){
 			duplicates[`item_${num}`] = 1;
@@ -21,4 +20,4 @@ for(let key in duplicates) {
 		resultArr.push(parseInt(key.replace('item_', '')))
 	}
 }
-console.log(resultArr);
+console.log("Result: "+resultArr);
